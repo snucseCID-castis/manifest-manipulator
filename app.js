20d91c8a2c3d973bb2d54e5fe2ac03577e80b0c2
JiyeonP <q0o0p@snu.ac.kr>
2024-04-06 19:09:47 +0900

    fix : rebase to main

--- app.js
+++ app.js
@@ -2,18 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//api request log
-app.use((req, res, next) => {
-  const start = Date.now();
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-    console.log(
-      `${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`
-    );
-  });
-  next();
-});
-
 app.get("/mm", (req, res) => {
   const m3u8Url = "http://110.35.173.88:19090/live.stream/ts.noll_master.m3u8";
 

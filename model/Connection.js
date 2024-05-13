b24b9a6762344d6b1f365e34186db8897e27b292
Darwin Jung <darwinj07@snu.ac.kr>
2024-05-13 14:04:07 +0900

    refactor(schema): Use default _id instead of sessionId

--- model/Connection.js
+++ model/Connection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const connectionSchema = new Schema({
-	sessionID: { type: String, required: true, unique: true },
 	cdn: { type: Schema.Types.ObjectId, ref: "CDN", required: true },
+	expiry: { type: Date, default: () => new Date(Date.now() + 10000) }, // currently set to 10 seconds from now. TODO: adjust according to media playlist request rate
 });
 
 module.exports = mongoose.model("Connection", connectionSchema);

b5a6e6fbcb990f20dd64697ffe1ad4cb77ebaf63
Darwin Jung <darwinj07@snu.ac.kr>
2024-05-13 16:27:17 +0900

    feat: Migrate playlist handling to playlistManager.js

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
-require("dotenv").config();
 const express = require("express");
-const axios = require("axios");
-const m3u8Parser = require("m3u8-parser");
 const cookieParser = require("cookie-parser");
 const ConnectionManager = require("./connectionManager");
+const PlaylistManager = require("./playlistManager");
 const app = express();
 
-const origin = process.env.ORIGIN_URL ||"http://110.35.173.88:19090/live.stream/";
 
-// TODO: CDN urls should be stored in DB
-const availableCDNs = [
-	// origin url, but for test
-	{
-		url: "http://110.35.173.88:19090",
-		playlist_uri: "/live.stream/",
-		traffic_uri: null,
-		detailed_traffic_uri: null,
-		type: "origin",
-	},
-	{
-		url: "http://www.castislive-cache2.com:19091",
-		playlist_uri: "/live.stream/",
-		traffic_uri: "/api/traffic",
-		detailed_traffic_uri: "/api/traffic/detail",
-		type: "cache",
-	},
-];
-
-// TODO: playlists should be stored in DB
-const masterPlaylists = {};
-const mediaPlaylists = new Set();
 const connectionManager = new ConnectionManager();
+const playlistManager = new PlaylistManager(); // updates playlists
 
 
 // logging middleware
@@ -56,136 +32,25 @@ app.use(async (req, res, next) =>{
 });
 
 
-// fetch master/media playlist
-async function fetchPlaylist(name) {
-	let content;
-	// TODO: DB query
-	if (name in masterPlaylists) {
-		content = masterPlaylists[name];
-	} else if (mediaPlaylists.has(name)) {
-		//cdn selection here
-		content = await fetchFromOrigin(name);
-	} else {
-		content = await storeNewMaster(name);
-	}
-	return content;
-}
 
-// fetch media playlist from origin
-async function fetchFromOrigin(name) {
-	const url = ensureAbsoluteUrl(origin, name);
-	try {
-		const response = await axios.get(url);
-		return response.data;
-	} catch (error) {
-		console.error("Failed to fetch media playlist from origin:", name);
-		return null;
-	}
-}
 
-// if master playlist is not in DB, fetch it from origin and store it and its media playlists in DB
-async function storeNewMaster(name) {
-	const url = ensureAbsoluteUrl(origin, name);
-	try {
-		const response = await axios.get(url);
-		if (response.status === 200) {
-			manifest = parseManifest(response.data);
-			if (!manifest.playlists) {
-				console.error("No playlists found in master playlist:", name);
-				return null;
-			}
-			// TODO: DB insert
-			masterPlaylists[name] = response.data;
-			for (const playlist of manifest.playlists) {
-				mediaPlaylists.add(playlist.uri);
-			}
-			const audios = manifest.mediaGroups.AUDIO;
-			for (const audio in audios) {
-				for (const track in audios[audio]) {
-					mediaPlaylists.add(audios[audio][track].uri);
-				}
-			}
-			return response.data;
-		}
-		console.error("Failed to fetch playlists from origin:", response.data); // invalid m3u8 request
-		return null;
-	} catch (error) {
-		console.error("Failed to fetch and store associated playlists:", name);
-		return null;
-	}
-}
-
-// parse manifest
-function parseManifest(m3u8) {
-	const parser = new m3u8Parser.Parser();
-	parser.push(m3u8);
-	parser.end();
-	return parser.manifest;
-}
-
-function ensureAbsoluteUrl(baseUrl, url) {
-	if (url.startsWith("http")) {
-		return url;
-	}
-	const urlObject = new URL(url, baseUrl);
-	return urlObject.href;
-}
-
-function ensureRelativeUrl(url) {
-	if (url.startsWith("http")) {
-		const urlObject = new URL(url);
-		return urlObject.pathname;
-	}
-	return url;
-}
-
-function reconstructMediaPlaylist(m3u8, cdnURL) {
-	manifest = parseManifest(m3u8);
-	let playlistContent = "#EXTM3U\n";
-
-	// TODO: can we add all the attributes automatically?
-	if (manifest.version) {
-		playlistContent += `#EXT-X-VERSION:${manifest.version}\n`;
-	}
-	if (manifest.mediaSequence) {
-		playlistContent += `#EXT-X-MEDIA-SEQUENCE:${manifest.mediaSequence}\n`;
-	}
-	if (manifest.targetDuration) {
-		playlistContent += `#EXT-X-TARGETDURATION:${manifest.targetDuration}\n`;
-	}
-
-	// substitute cdn url to every segment uri
-	// TODO: may need to vary cdn for every segment
-	if (manifest?.segments) {
-		for (const segment of manifest.segments) {
-			playlistContent += `#EXTINF:${segment.duration.toFixed(
-				3,
-			)},\n${ensureAbsoluteUrl(cdnURL, segment.uri)}\n`;
-		}
-	}
-
-	return playlistContent;
-}
-
-// TODO: dynamic CDN selection
-async function selectCDN() {
-	cdn = availableCDNs[1];
-	return cdn.url + cdn.playlist_uri;
-}
 
 app.get("/:pathname", async (req, res) => {
-	const name = req.params.pathname;
-	const m3u8 = await fetchPlaylist(name); 
-	if (name in masterPlaylists) {
-		res.header("Content-Type", "application/vnd.apple.mpegurl");
-		return res.send(m3u8);
+	const playlistName = req.params.pathname;
+	const playlistContent = await playlistManager.fetchPlaylist(playlistName);
+	if (!playlistContent) {
+		return res.status(404).send("Not Found");
 	}
-	if (mediaPlaylists.has(name)) {
-		cdnURL = await selectCDN();
-		res.header("Content-Type", "application/vnd.apple.mpegurl");
-		return res.send(reconstructMediaPlaylist(m3u8, cdnURL));
-	}
-	res.status(400).send("Bad Request");
+	
+	res.header("Content-Type", "application/vnd.apple.mpegurl");
+	return res.send(playlistContent);
+	
+	// if (mediaPlaylists.has(name)) {
+	// 	cdnURL = await selectCDN();
+	// 	res.header("Content-Type", "application/vnd.apple.mpegurl");
+	// 	return res.send(reconstructMediaPlaylist(m3u8, cdnURL));
+	// }
+	// res.status(400).send("Bad Request");
 });
 
 module.exports = { app };
---
+++ playlistManager.js
@@ -0,0 +1,158 @@
+require("dotenv").config();
+const MasterPlaylist = require("./models/MasterPlaylist");
+const MediaPlaylist = require("./models/MediaPlaylist");
+const axios = require("axios");
+const m3u8Parser = require("m3u8-parser");
+
+const origin = process.env.ORIGIN_URL ||"http://110.35.173.88:19090/live.stream/";
+
+
+// TODO: CDN urls should be stored in DB
+const availableCDNs = [
+	// origin url, but for test
+	{
+		url: "http://110.35.173.88:19090",
+		playlist_uri: "/live.stream/",
+		traffic_uri: null,
+		detailed_traffic_uri: null,
+		type: "origin",
+	},
+	{
+		url: "http://www.castislive-cache2.com:19091",
+		playlist_uri: "/live.stream/",
+		traffic_uri: "/api/traffic",
+		detailed_traffic_uri: "/api/traffic/detail",
+		type: "cache",
+	},
+];
+
+async function updateAndGetMasterPlaylists() {
+    const masterPlaylists = await MasterPlaylist.find();
+    for (const playlist of masterPlaylists) {
+        const content = await fetchFromOrigin(playlist.name);
+        playlist.contents = content;
+        await playlist.save();
+    }
+    return masterPlaylists;
+}
+
+async function updateandGetMediaPlaylists() {
+    // Purge all media playlists
+    await MediaPlaylist.deleteMany();
+
+    const masterPlaylists = await MasterPlaylist.find();
+    const createdMediaPlaylists = []; // Array to store created media playlists
+
+    for (const playlist of masterPlaylists) {
+        const content = playlist.contents;
+        const manifest = parseManifest(content);
+        for (const mediaPlaylist of manifest.playlists) {
+            const name = ensureRelativeUrl(mediaPlaylist.uri);
+            const createdPlaylist = await MediaPlaylist.create({ name });
+            createdMediaPlaylists.push(createdPlaylist); // Store the created playlist
+        }
+    }
+
+    return createdMediaPlaylists;
+}
+
+
+
+// fetch media playlist from origin
+async function fetchFromOrigin(name) {
+	const url = ensureAbsoluteUrl(origin, name);
+	try {
+		const response = await axios.get(url);
+		return response.data;
+	} catch (error) {
+		console.error("Failed to fetch media playlist from origin:", name);
+		return null;
+	}
+}
+
+function parseManifest(m3u8) {
+	const parser = new m3u8Parser.Parser();
+	parser.push(m3u8);
+	parser.end();
+	return parser.manifest;
+}
+
+function ensureAbsoluteUrl(baseUrl, url) {
+	if (url.startsWith("http")) {
+		return url;
+	}
+	const urlObject = new URL(url, baseUrl);
+	return urlObject.href;
+}
+
+
+function ensureRelativeUrl(url) {
+	if (url.startsWith("http")) {
+		const urlObject = new URL(url);
+		return urlObject.pathname;
+	}
+	return url;
+}
+
+
+
+
+function reconstructMediaPlaylist(m3u8, cdnURL) {
+	manifest = parseManifest(m3u8);
+	let playlistContent = "#EXTM3U\n";
+
+	// TODO: can we add all the attributes automatically?
+	if (manifest.version) {
+		playlistContent += `#EXT-X-VERSION:${manifest.version}\n`;
+	}
+	if (manifest.mediaSequence) {
+		playlistContent += `#EXT-X-MEDIA-SEQUENCE:${manifest.mediaSequence}\n`;
+	}
+	if (manifest.targetDuration) {
+		playlistContent += `#EXT-X-TARGETDURATION:${manifest.targetDuration}\n`;
+	}
+
+	// substitute cdn url to every segment uri
+	// TODO: may need to vary cdn for every segment
+	if (manifest?.segments) {
+		for (const segment of manifest.segments) {
+			playlistContent += `#EXTINF:${segment.duration.toFixed(
+				3,
+			)},\n${ensureAbsoluteUrl(cdnURL, segment.uri)}\n`;
+		}
+	}
+
+	return playlistContent;
+}
+
+// TODO: modularize 
+async function selectCDN() {
+	cdn = availableCDNs[1];
+	return cdn.url + cdn.playlist_uri;
+}
+
+class PlaylistManager {
+    constructor() { // On init, update master and media playlists
+        this.masterPlaylists = updateAndGetMasterPlaylists();
+        this.mediaPlaylists = updateandGetMediaPlaylists();
+    }
+    async fetchPlaylist(name) {
+        //check if document with name exists in masterPlaylists or mediaPlaylists
+        const masterPlaylist = await MasterPlaylist.findOne({ name });
+        if (masterPlaylist) {
+            return masterPlaylist.contents;
+        }
+        const mediaPlaylist = await MediaPlaylist.findOne({ name });
+        if (!mediaPlaylist) {
+            return null;
+        }
+        CDNURL = await selectCDN();
+        // TODO: fetch from selected CDN
+        contents = await fetchFromOrigin(mediaPlaylist.name);
+        return reconstructMediaPlaylist(contents, CDNURL);
+
+
+
+    }
+
+}

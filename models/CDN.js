a52886726a7b1eb0719a95313e7020225e76b0d3
jhm1313 <jhm1313@naver.com>
2024-05-18 17:41:11 +0900

    chore: rename CDN collection

--- models/CDN.js
+++ models/CDN.js
@@ -5,6 +5,11 @@ const cdnSchema = new Schema({
 	url: { type: String, required: true, unique: true },
 	traffic_uri: { type: String, required: true },
 	type: { type: String, required: true, enum: ["cache", "cloudfront"] },
+	status: {
+		connection_count: { type: Number, default: 0 },
+		tps: { type: Number, default: 0 },
+		bps: { type: Number, default: 0 },
+	},
 });
 
-module.exports = mongoose.model("CDN", cdnSchema);
+module.exports = mongoose.model("CDN", cdnSchema, "CDN");

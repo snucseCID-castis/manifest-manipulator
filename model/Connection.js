fd92a17aa82955d9356832e3226f34feca558898
Darwin Jung <darwinj07@snu.ac.kr>
2024-05-23 12:54:15 +0900

    feat(DB): Set TTL for expiry

--- model/Connection.js
+++ model/Connection.js
@@ -6,4 +6,6 @@ const connectionSchema = new Schema({
 	expiry: { type: Date, default: () => new Date(Date.now() + 10000) }, // currently set to 10 seconds from now. TODO: adjust according to media playlist request rate
 });
 
+connectionSchema.index({ expiry: 1 }, { expireAfterSeconds: 300 });
+
 module.exports = mongoose.model("Connection", connectionSchema);

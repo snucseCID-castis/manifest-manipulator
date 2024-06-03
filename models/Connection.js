a2c8b82c90b5eda7ff7559d64045a5d838a85ebd
Darwin Jung <darwinj07@snu.ac.kr>
2024-06-04 02:21:43 +0900

    feat: Add requestLogs property in Connection

--- models/Connection.js
+++ models/Connection.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const requestLog = new Schema({
+	mediaPlaylist: {
+		type: Schema.Types.ObjectId,
+		ref: "MediaPlaylist",
+		required: true,
+	},
+	time: { type: Date, default: Date.now },
+});
+
 const connectionSchema = new Schema({
 	cdn: { type: Schema.Types.ObjectId, ref: "CDN", required: false },
 	expiry: { type: Date, default: () => new Date(Date.now() + 10000) }, // currently set to 10 seconds from now. TODO: adjust according to media playlist request rate
+	requestLogs: { type: [requestLog], default: [] },
 });
 
 connectionSchema.index({ expiry: 1 }, { expireAfterSeconds: 300 });
